f68e38d265165996e969473e74b9f7a699f139d8
Duncan McGreggor <oubiwann@gmail.com>
2015-09-30 18:52:02 -0500

    Updated deps.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,22 +1,18 @@
 (defproject example-rest-api "0.1.0-SNAPSHOT"
   :description "An Example REST API Server for Clojure"
   :url "http://localhost/example-api"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.3"]
-                 [org.clojure/tools.cli "0.2.4"]
-                 [org.clojure/tools.logging "0.2.6"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [clj-http "0.7.7"]
-                 [compojure "1.1.5"]
-                 [http-kit "2.1.11"]
-                 [ring/ring-core "1.2.0"]
-                 [ring/ring-devel "1.2.0"]
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.17"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-devel "1.4.0"]
                  [ring/ring-json "0.2.0"]]
-  :plugins [[lein-ring "0.8.5"]
-            [lein-exec "0.3.1"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-exec "0.3.5"]]
   :ring {:handler example-rest-api.handler/app}
-  :main example-rest-api.main
-  :profiles
-  {:testing
-    {:dependencies [[ring-mock "0.1.5"]
-                    [clj-http-fake "0.4.1"]
-                    [midje "1.5.1"]]}})
+  :main example-rest-api.main)

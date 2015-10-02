d0e125e8ea7910d5ea641a2cd461b6d0c9845d77
Duncan McGreggor <oubiwann@gmail.com>
2015-10-02 14:00:35 -0500

    Added total mem and placeholders for remaining data.
    Confiormed that this is working in JMeter run.

--- clojure/src/example_rest_api/management.clj
+++ clojure/src/example_rest_api/management.clj
@@ -8,4 +8,18 @@
     [:status
      [:jvm
       [:memory {:free (str (. (Runtime/getRuntime) freeMemory))
-                :total "?"}]]])))
+                :total (str (. (Runtime/getRuntime) totalMemory))
+                :max "0"}]
+      [:connector {:name "test-rest"}
+       [:threadInfo {:maxThreads "0"
+                     :minSpareThreads "0"
+                     :maxSpareThreads "0"
+                     :currentThreadCount "0"
+                     :currentThreadsBusy "0"}]
+       [:requestInfo {:maxTime "0"
+                      :processingTime "0"
+                      :requestCount "0"
+                      :errorCount "0"
+                      :bytesReceived "0"
+                      :bytesSent "0"}]
+       [:workers]]]])))
